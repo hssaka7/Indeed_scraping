f63c567c13008af435d334beb9d6b514ec443d5f
hssaka7 <hssaka7@gmail.com>
2024-02-09 15:38:38 -0600

    Added database orm models

--- api/routes.py
+++ api/routes.py
@@ -1,1 +1,14 @@
-print("API coming soon")
+from fastapi import FastAPI
+
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {'code':'success', 'message':"Hello World!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run('routes:app', host='127.0.0.1', port=8000, reload=True)
+    print("API coming soon http://127.0.0.1:8000/")
