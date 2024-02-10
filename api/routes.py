b541af159da42186cbb3790f7e53d2a591e81f19
hssaka7 <hssaka7@gmail.com>
2024-02-10 12:01:25 -0600

    Working api for geting feeds

--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from tortoise.contrib.fastapi import register_tortoise
+from typing import List
 
 import uvicorn
+
+from schema import Feed,Result_Pydantic_List, Feed_Pydantic_List, Feed_Pydantic
 
 app = FastAPI()
 
@@ -8,6 +12,19 @@ app = FastAPI()
 async def root():
     return {'code':'success', 'message':"Hello World!"}
 
+@app.get("/feeds", response_model=List[Feed_Pydantic])
+async def get_feeds():
+    all_feed = Feed.all()
+    print(all_feed)
+    return await Feed_Pydantic.from_queryset(Feed.all())
+
+register_tortoise(
+    app,
+    db_url="sqlite://scrapers.db",
+    modules={"models": ["schema"]},
+    generate_schemas=False,
+    add_exception_handlers=True,
+)
 
 if __name__ == "__main__":
     uvicorn.run('routes:app', host='127.0.0.1', port=8000, reload=True)
---
+++ api/schema.py
@@ -0,0 +1,56 @@
+from tortoise.models import Model
+from tortoise import fields, run_async, Tortoise 
+from tortoise.contrib.pydantic import pydantic_model_creator, pydantic_queryset_creator
+
+class Feed(Model):
+    id = fields.IntField(pk=True, index= True)
+    name = fields.CharField(max_length=20)
+    description = fields.CharField(max_length=100)
+    frequency = fields.CharField(max_length=10)
+
+    workers: fields.ReverseRelation["Worker"]
+   
+
+class Worker(Model):
+    id = fields.CharField(pk=True,max_length=50, index =True)
+    status = fields.CharField(max_length=20)
+    created_by = fields.CharField(max_length=20)
+    created_at = fields.DatetimeField()
+    
+    feed: fields.ForeignKeyRelation["Feed"]= fields.ForeignKeyField(
+        "models.Feed", related_name="workers", description="The feed under which workers are running "
+    )
+    results: fields.ReverseRelation["Result"]
+
+    # results: fields.ManyToManyRelation["Result"] = fields.ManyToManyField(
+    #     "models.Result", related_name="workers", through="worker_result"
+    # )
+
+
+    
+    
+class Result(Model):
+    id = fields.IntField(pk=True,index =True)
+    job_id = fields.CharField(max_length=50)
+    key_name = fields.CharField(max_length=50)
+    value = fields.CharField(max_length=50)
+    worker: fields.ForeignKeyRelation["Worker"] = fields.ForeignKeyField(
+        "models.Worker", related_name="results", description="The results saved by the workers"
+    )
+
+Feed_Pydantic = pydantic_model_creator(Feed)
+Feed_Pydantic_List= pydantic_queryset_creator(Feed)
+Result_Pydantic_List = pydantic_queryset_creator(Result)
+
+async def connectToDatabase():
+    await Tortoise.init(
+        db_url='sqlite://scrapers.db',
+        modules={'models': ['__main__']}
+    )
+    
+    # Uncomment this to initialize db. 
+    # await Tortoise.generate_schemas()
+    
+
+if __name__ == "__main__":
+    run_async(connectToDatabase())
